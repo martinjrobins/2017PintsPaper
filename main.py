02e1b271f8a9db9754d4f0d72d0d9032ca1553a8
Martin Robinson <martinjrobins@gmail.com>
2018-02-21 17:04:52 +0000

    add some mcmc methods

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pints
-from tasks import optimise_sampler
+from tasks import optimise_sampler, mcmc_sampler
 import argparse
 from math import floor
 from subprocess import call
@@ -15,6 +15,7 @@ parameters = [[0.015, 500.0],
 times = [np.linspace(0, 1000, 1000),
          pints.toy.HodgkinHuxleyIKModel().suggested_times()]
 optimisers = [pints.CMAES, pints.PSO, pints.XNES, pints.SNES]
+mcmcs = [pints.AdaptiveCovarianceMCMC, pints.DifferentialEvolutionMCMC]
 noise_levels = [0.01, 0.1]
 num_samples = 5
 
@@ -32,7 +33,7 @@ if __name__ == "__main__":
                         help='run entire matrix')
 
     args = parser.parse_args()
-    size = len(models) * len(optimisers) * len(noise_levels)
+    size = len(models) * (len(optimisers) + len(mcmcs)) * len(noise_levels)
 
     if args.max:
         print(size - 1)
@@ -42,12 +43,16 @@ if __name__ == "__main__":
         i = args.integer
         ni = i % len(noise_levels)
         i = int(floor(i / len(noise_levels)))
-        no = i % len(optimisers)
-        i = int(floor(i / len(optimisers)))
+        no = i % (len(optimisers) + len(mcmcs))
+        i = int(floor(i / (len(optimisers) + len(mcmcs))))
         nm = i % len(models)
         print('running matrix (%d,%d,%d)' % (nm, no, ni))
-        output = optimise_sampler(num_samples, optimisers[no], models[nm],
+        if no >= len(optimisers):
+            output = mcmc_sampler(num_samples, mcmcs[no - len(optimisers)], models[nm],
                                   noise_levels[ni], times[nm], parameters[nm])
+        else:
+            output = optimise_sampler(num_samples, optimisers[no], models[nm],
+                                      noise_levels[ni], times[nm], parameters[nm])
         fname = 'output_%d_%d_%d.pickle' % (nm, no, ni)
         print('writing ' + fname)
         pickle.dump(output, open(fname, 'wb'))
@@ -65,15 +70,33 @@ if __name__ == "__main__":
                         pickle.dump(output, open(fname, 'wb'))
                     except Exception:
                         pass
+                for no, mcmc in enumerate(mcmcs):
+                    print('running matrix (%d,%d,%d)' % (nm, no, ni))
+                    try:
+                        output = mcmc_sampler(num_samples, optimiser,
+                                              model, noise, times[nm],
+                                              parameters[nm])
+                        fname = 'output_%d_%d_%d.pickle' % (
+                            nm, no + len(optimisers), ni)
+                        print('writing ' + fname)
+                        pickle.dump(output, open(fname, 'wb'))
+                    except Exception:
+                        pass
+
     elif args.plot:
         f = plt.figure()
         y = range(len(models))
         y_labels = [m.__name__ for m in models]
         x = range(len(optimisers))
         x_labels = [o.__name__ for o in optimisers]
+        x_mcmc = range(len(mcmcs))
+        x_mcmc_labels = [m.__name__ for m in mcmcs]
         for ni, noise in enumerate(noise_levels):
             score = np.zeros((len(models), len(optimisers), num_samples))
             time = np.zeros((len(models), len(optimisers), num_samples))
+            rhat = np.zeros((len(models), len(mcmcs), num_samples))
+            ess = np.zeros((len(models), len(mcmcs), num_samples))
+            time_mcmc = np.zeros((len(models), len(mcmcs), num_samples))
             for nm, model in enumerate(models):
                 for no, optimiser in enumerate(optimisers):
                     fname = 'output_%d_%d_%d.pickle' % (
@@ -87,6 +110,20 @@ if __name__ == "__main__":
                     else:
                         score[nm, no, :] = float('nan')
                         time[nm, no, :] = float('nan')
+                for no, mcmc in enumerate(mcmcs):
+                    fname = 'output_%d_%d_%d.pickle' % (
+                        nm, no + len(optimisers), ni)
+                    print('reading ' + fname)
+                    if os.path.exists(fname):
+                        output = pickle.load(open(fname, 'rb'))
+                        assert(len(output[:, 1] == num_samples))
+                        rhat[nm, no, :] = output[:, 0]
+                        ess[nm, no, :] = output[:, 1]
+                        time_mcmc[nm, no, :] = output[:, 2]
+                    else:
+                        rhat[nm, no, :] = float('nan')
+                        ess[nm, no, :] = float('nan')
+                        time_mcmc[nm, no, :] = float('nan')
 
             for nm, model in enumerate(models):
                 min_score = np.min(score[nm, :, :], axis=(0, 1))
@@ -97,6 +134,18 @@ if __name__ == "__main__":
                 max_time = np.max(time[nm, :, :], axis=(0, 1))
                 time[nm, :, :] = (time[nm, :, :] -
                                   min_time) / (max_time - min_time)
+                min_rhat = np.min(rhat[nm, :, :], axis=(0, 1))
+                max_rhat = np.max(rhat[nm, :, :], axis=(0, 1))
+                rhat[nm, :, :] = (rhat[nm, :, :] -
+                                  min_rhat) / (max_rhat - min_rhat)
+                min_ess = np.min(ess[nm, :, :], axis=(0, 1))
+                max_ess = np.max(ess[nm, :, :], axis=(0, 1))
+                ess[nm, :, :] = (ess[nm, :, :] -
+                                 min_ess) / (max_ess - min_ess)
+                min_time_mcmc = np.min(time_mcmc[nm, :, :], axis=(0, 1))
+                max_time_mcmc = np.max(time_mcmc[nm, :, :], axis=(0, 1))
+                time_mcmc[nm, :, :] = (time_mcmc[nm, :, :] -
+                                       min_time_mcmc) / (max_time_mcmc - min_time_mcmc)
 
             plt.clf()
             imshow = plt.imshow(np.mean(score, axis=2), cmap='RdYlBu_r',
@@ -130,3 +179,52 @@ if __name__ == "__main__":
             plt.colorbar(label='time (min)')
             plt.tight_layout()
             plt.savefig('time_mean_with_noise_%d.pdf' % ni)
+
+            plt.clf()
+            imshow = plt.imshow(np.mean(rhat, axis=2), cmap='RdYlBu_r',
+                                interpolation='nearest')
+            plt.xticks(x_mcmc, x_mcmc_labels, rotation='vertical')
+            plt.yticks(y, y_labels)
+            plt.colorbar(label='rhat (mean)')
+            plt.tight_layout()
+            plt.savefig('rhat_mean_with_noise_%d.pdf' % ni)
+            plt.clf()
+            imshow = plt.imshow(np.min(rhat, axis=2), cmap='RdYlBu_r',
+                                interpolation='nearest')
+            plt.xticks(x_mcmc, x_mcmc_labels, rotation='vertical')
+            plt.yticks(y, y_labels)
+            plt.colorbar(label='rhat (min)')
+            plt.tight_layout()
+            plt.savefig('rhat_min_with_noise_%d.pdf' % ni)
+            plt.clf()
+            plt.imshow(np.mean(ess, axis=2),
+                       cmap='RdYlBu_r', interpolation='nearest')
+            plt.xticks(x_mcmc, x_mcmc_labels, rotation='vertical')
+            plt.yticks(y, y_labels)
+            plt.colorbar(label='ess (mean)')
+            plt.tight_layout()
+            plt.savefig('ess_mean_with_noise_%d.pdf' % ni)
+            plt.clf()
+            plt.imshow(np.min(ess, axis=2),
+                       cmap='RdYlBu_r', interpolation='nearest')
+            plt.xticks(x_mcmc, x_mcmc_labels, rotation='vertical')
+            plt.yticks(y, y_labels)
+            plt.colorbar(label='ess (min)')
+            plt.tight_layout()
+            plt.savefig('ess_mean_with_noise_%d.pdf' % ni)
+            plt.clf()
+            plt.imshow(np.mean(time_mcmc, axis=2),
+                       cmap='RdYlBu_r', interpolation='nearest')
+            plt.xticks(x_mcmc, x_mcmc_labels, rotation='vertical')
+            plt.yticks(y, y_labels)
+            plt.colorbar(label='time_mcmc (mean)')
+            plt.tight_layout()
+            plt.savefig('time_mcmc_mean_with_noise_%d.pdf' % ni)
+            plt.clf()
+            plt.imshow(np.min(time_mcmc, axis=2),
+                       cmap='RdYlBu_r', interpolation='nearest')
+            plt.xticks(x_mcmc, x_mcmc_labels, rotation='vertical')
+            plt.yticks(y, y_labels)
+            plt.colorbar(label='time_mcmc (min)')
+            plt.tight_layout()
+            plt.savefig('time_mcmc_mean_with_noise_%d.pdf' % ni)
--- tasks.py
+++ tasks.py
@@ -43,6 +43,39 @@ def optimise(sample, optimiser, model, noise, times, real_parameters):
     return found_parameters, found_value, end - start
 
 
+def mcmc(sample, mcmc_method, model, noise, times, real_parameters):
+    print('Running sample' + str(sample))
+    the_model = model()
+    values = the_model.simulate(real_parameters, times)
+    value_range = np.max(values) - np.min(values)
+    values += np.random.normal(0, noise * value_range, values.shape)
+    problem = pints.SingleSeriesProblem(the_model, times, values)
+    log_likelihood = pints.UnknownNoiseLogLikelihood(problem)
+    lower = np.array(
+        [x / 10.0 for x in list(real_parameters) + [value_range * noise]])
+    upper = np.array(
+        [x * 10.0 for x in list(real_parameters) + [value_range * noise]])
+    middle = [0.5 * (u + l) for l, u in zip(lower, upper)]
+    sigma = [u - l for l, u in zip(lower, upper)]
+    log_prior = pints.UniformLogPrior(lower, upper)
+    log_posterior = pints.LogPosterior(log_likelihood, log_prior)
+    xs = [x * (upper - lower) + lower for x in np.random.uniform(size=3)]
+    mcmc = pints.MCMCSampling(log_posterior, 3, xs, method=mcmc_method)
+    # mcmc.set_max_iterations(10000)
+    mcmc.set_verbose(False)
+
+    start = timer()
+    chains = mcmc.run()
+    end = timer()
+
+    rhat = np.max(pints._diagnostics.rhat_all_params(chains))
+    ess = 0
+    for chain in chains:
+        ess += pints._diagnostics.effective_sample_size(chain)
+    ess = np.min(ess)
+    return rhat, ess, end - start
+
+
 def optimise_sampler(num_samples, optimiser, model, noise, times, real_parameters):
     p = multiprocessing.Pool(multiprocessing.cpu_count())
     args = zip(range(num_samples), repeat(optimiser), repeat(
@@ -51,5 +84,9 @@ def optimise_sampler(num_samples, optimiser, model, noise, times, real_parameter
     return np.array(results)
 
 
-if __name__ == "__main__":
-    optimise(pints.PSO, pints.toy.HodgkinHuxleyIKModel, 10.0)
+def mcmc_sampler(num_samples, mcmc_method, model, noise, times, real_parameters):
+    p = multiprocessing.Pool(multiprocessing.cpu_count())
+    args = zip(range(num_samples), repeat(mcmc_method), repeat(
+        model), repeat(noise), repeat(times), repeat(real_parameters))
+    results = p.starmap(mcmc, args)
+    return np.array(results)
